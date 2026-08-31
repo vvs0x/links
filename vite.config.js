fff1abb9916ef3ddb193d5356fcd04d81394cdca
vvs0x <vschwarz@ik.me>
2026-08-31 21:53:55 +0300

    change path to '/' for external domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/linktree/', // This MUST match your exact GitHub repository name
+  base: '/', 
 })
