ef37155c7262f67447e6251497872c140c5c226a
vvs0x <vschwarz@ik.me>
2026-08-31 13:29:26 +0300

    fix base path for pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: '/linktree/', // This MUST match your exact GitHub repository name
 })
